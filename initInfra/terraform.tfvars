5300f503fa0c102c03c629940289b429d12ab2fa
Hariharan Kurathallvar Sriram <hariharan.kurathallvar-sriram@stfc.ac.uk>
2026-08-04 11:52:56 +0100

    remove default values

--- initInfra/terraform.tfvars
+++ initInfra/terraform.tfvars
@@ -1,9 +1,9 @@
 # Single Environment Configuration
-stack_name           = "k8s-ha"
+stack_name           = "your_name"
 cloud_name           = "openstack"
 clouds_yaml_path     = "../secrets/clouds.yaml"
 ssh_private_key_file = "../secrets/private_key.pem"
-key_pair_name        = "FASE_CLOUD_DEV"
+key_pair_name        = "your_key_pair_name"
 
 # Cluster Topology
 controlplane_count = 3
@@ -12,8 +12,8 @@ lb_count           = 1
 
 # Node Flavors
 controlplane_flavor = "l3.nano"
-worker_flavor       = "l3.tiny"
+worker_flavor       = "l3.nano"
 lb_flavor           = "l3.nano"
 
 # Load Balancer Floating IP
-lb_fip_address = "130.246.81.27"
+lb_fip_address = "your_fip"
--- initInfra/variables.tf
+++ initInfra/variables.tf
@@ -13,7 +13,6 @@ variable "clouds_yaml_path" {
 variable "stack_name" {
   description = "Stack name for resource naming and config lookup"
   type        = string
-  default     = "k8s"
 }
 
 variable "controlplane_count" {
@@ -37,23 +36,19 @@ variable "ssh_private_key_file" {
 variable "key_pair_name" {
   description = "Name of the OpenStack SSH key pair"
   type        = string
-  default     = "FASE_CLOUD_DEV"
 }
 
 variable "controlplane_flavor" {
   description = "Flavor name for control plane nodes"
   type        = string
-  default     = "l3.tiny"
+  default     = "l3.nano"
 }
 
 variable "worker_flavor" {
   description = "Flavor name for worker nodes"
   type        = string
-  default     = "l3.tiny"
+  default     = "l3.nano"
 }
-
-
-
 
 variable "lb_network_cidr" {
   description = "The CIDR block for the dedicated LoadBalancer network"
@@ -76,5 +71,4 @@ variable "lb_flavor" {
 variable "lb_fip_address" {
   description = "The Floating IP address to assign to the application load balancer"
   type        = string
-  default     = "130.246.81.27"
 }
